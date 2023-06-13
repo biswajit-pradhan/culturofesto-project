997607a555578e34c5a5e844aae475b69658d98a
Pranil Kate <pranilkate2000@gmail.com>
2023-06-13 15:39:19 +0530

    log frontend and get eventbyeventid connected

--- cultrofesto/src/Components/EventDetailsPage.jsx
+++ cultrofesto/src/Components/EventDetailsPage.jsx
@@ -2,48 +2,10 @@ import React, { useState, useEffect } from "react";
 import { NavLink } from "react-bootstrap";
 import { useNavigate, useParams } from "react-router-dom";
 
-const eventData = [
-  {
-    eventId: 1,
-    eventName: "Dasahara",
-    eventCapacity: 5,
-    deleteStatus: false,
-  },
-  {
-    eventId: 2,
-    eventName: "Ratha Yatra",
-    eventCapacity: 5,
-    deleteStatus: false,
-  },
-  {
-    eventId: 3,
-    eventName: "Danda Nrutya",
-    eventCapacity: 5,
-    deleteStatus: false,
-  },
-  {
-    eventId: 4,
-    eventName: "Jatra",
-    eventCapacity: 5,
-    deleteStatus: false,
-  },
-  {
-    eventId: 5,
-    eventName: "Dasahara",
-    eventCapacity: 5,
-    deleteStatus: false,
-  },
-  {
-    eventId: 6,
-    eventName: "Ratha Yatra",
-    eventCapacity: 5,
-    deleteStatus: false,
-  },
-];
-
 const EventDetailsPage = () => {
   const { eventId } = useParams();
   const [event, setEvent] = useState(null);
+  const [eventImage, setEventImage] = useState(null);
 
   const navigate = useNavigate();
   const handleBack = () => {
@@ -53,10 +15,15 @@ const EventDetailsPage = () => {
   useEffect(() => {
     const fetchEventDetails = async () => {
       try {
-        const eventDetails = eventData.find(
-          (event) => event.eventId === parseInt(eventId)
-        );
-        setEvent(eventDetails);
+        const eventResponse = await fetch(`http://localhost:9004/api/event/${eventId}/0`);
+        const eventData = await eventResponse.json();
+        setEvent(eventData);
+
+        const imageResponse = await fetch(`http://localhost:9004/api/event/${eventId}/1`);
+        if (imageResponse.ok) {
+          const imageBlob = await imageResponse.blob();
+          setEventImage(URL.createObjectURL(imageBlob));
+        }
       } catch (error) {
         console.error("Error fetching event details:", error);
       }
@@ -92,11 +59,26 @@ const EventDetailsPage = () => {
         >
           <div className="navlink_event_details">
             <div className="event_details_data">
-              <h1>{event.eventName}</h1>
-              <p>Event ID: {event.eventId}</p>
+              {eventImage && (
+                <img
+                  src={eventImage}
+                  alt="Event"
+                  style={{ width: '500px', height: '500px', borderRadius: '5%' }}
+                />
+              )}
+              <h1 style={{color:"ThreeDLightShadow" }}>{event.eventName}</h1>
+              <p>Event Date: {event.eventDate}</p>
+              <p>Registration Open Date: {event.registrationOpenDate}</p>
+              <p>Registration Close Date: {event.registrationCloseDate}</p>
+              <p>Event Start Time: {event.eventStartTime}</p>
+              <p>Event Close Time: {event.eventCloseTime}</p>
+              <p>Registration Fee : {event.registrationFee}</p>
+              <p>Event Capacity: {event.eventCapacity}</p>
+              <p>Breakfast Price: {event.breakfastPrice}</p>
+              <p>Lunch Price: {event.lunchPrice}</p>
+              <p>Dinner Price: {event.dinnerPrice}</p>
               <p>Hurry only {event.eventCapacity} tickets remaining...</p>
             </div>
-
             <NavLink
               className="btn btn-primary nav_link"
               onClick={handleBookTicket}
